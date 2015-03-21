a278b579edf7f8ef9097fb806b6266a792ec6509
Michael Wong <michael.khw.dev@gmail.com>
2015-03-21 21:02:57 +0800

    Setting up Playground Zero project

---
+++ playground-zero/src/main/java/com/github/michaelkhw/playground/zero/App.java
@@ -0,0 +1,10 @@
+package com.github.michaelkhw.playground.zero;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to playground");
+    }
+}
